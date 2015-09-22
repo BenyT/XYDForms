4d6b8b91d3db06d95a4a0cd8216758dd0e3ca989
Diogo Mateus <diogo.tpr@gmail.com>
2015-09-22 22:42:47 +0100

    Added new fields to delegate. Created the XYFCollectionViewFlowLayout to support more designs

--- YForms/YFormDelegates.swift
+++ YForms/YFormDelegates.swift
@@ -8,6 +8,84 @@
 
 import UIKit
 
+class XYFCollectionViewFlowLayout: UICollectionViewFlowLayout {
+    
+    var formController: YFormController!
+    private var cache = [UICollectionViewLayoutAttributes]()
+    
+    override func prepareLayout() {
+        
+        // TODO: SUPPORT MORE SECTIONS
+        
+        var yOffset: CGFloat = 0
+        var xOffset: CGFloat = 0
+        var currentWidthPercentage: CGFloat = 0
+        
+        
+        var numberOfInnerRows: CGFloat = 0
+        var currentInnerRow: CGFloat = 0
+        
+        
+        for section in 0..<collectionView!.numberOfSections() {
+            for item in 0..<collectionView!.numberOfItemsInSection(section) {
+                let indexPath = NSIndexPath(forItem: item, inSection: section)
+                
+                let field = formController.collectionViewDataSource!.sections[section].fields[item]
+                
+                let width = collectionView!.frame.width * field.percentageWidth
+                let height = formController.fieldHeight * field.heightMultiplier
+                
+                let frame = CGRect(x: xOffset, y: yOffset, width: width, height: height)
+
+                
+                // 3rd onwards - 8-9-10
+                if currentInnerRow > 0 {
+                    currentInnerRow++
+                    yOffset += height
+                    
+                }
+                
+                // 7
+                if field.heightMultiplier > 1 && numberOfInnerRows == 0 {
+                    numberOfInnerRows = field.heightMultiplier
+                    currentInnerRow = 1
+                    yOffset += height / field.heightMultiplier
+                }
+                
+                xOffset += width
+                
+                if xOffset == collectionView!.frame.width || currentInnerRow > 0 {
+                    xOffset = 0
+                    if numberOfInnerRows == currentInnerRow {
+                        yOffset += height
+                        currentInnerRow = 0
+                        numberOfInnerRows = 0
+                    }
+                } else if currentWidthPercentage > 1 {
+                    assertionFailure("A row can't have a width percentage superior to 1")
+                }
+                
+                let attributes = UICollectionViewLayoutAttributes(forCellWithIndexPath: indexPath)
+                attributes.frame = CGRectInset(frame, 0, 0)
+                cache.append(attributes)
+            }
+        }
+    }
+    
+    override func layoutAttributesForElementsInRect(rect: CGRect) -> [AnyObject]? {
+        
+        var layoutAttributes = [UICollectionViewLayoutAttributes]()
+        
+        for attributes in cache {
+            if CGRectIntersectsRect(attributes.frame, rect) {
+                layoutAttributes.append(attributes)
+            }
+        }
+        return layoutAttributes
+    }
+    
+}
+
 // This CollectionView delegate's purpose is to calculate the size of each cell, taking into account the percentage width of each field.
 class YFCollectionViewDelegate: NSObject, UICollectionViewDelegate {
     
@@ -19,30 +97,67 @@ class YFCollectionViewDelegate: NSObject, UICollectionViewDelegate {
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         let field = formController.collectionViewDataSource!.sections[indexPath.section].fields[indexPath.row]
-        return CGSizeMake(collectionView.frame.width * CGFloat(field.percentageWidth), formController.fieldHeight)
+        return CGSizeMake(collectionView.frame.width * CGFloat(field.percentageWidth), formController.fieldHeight * field.heightMultiplier)
     }
     
 }
 
-class YFCollectionViewDataSource: NSObject, UICollectionViewDataSource {
-    
-    static let cellIdentifier: String = "YFFieldCellIdentifier"
-    static let headerIdentifier: String = "YFFieldHeaderIdentifier"
+class YFCollectionViewDataSource: NSObject, UICollectionViewDataSource, TagWriteViewDelegate, YFPhotoFieldCellDelegate, XYZSwitchFieldCellDelegate, XYFDatePickerViewControllerDelegate {
     
     var sections: [YFSection] = []
     var textFieldDelegate: YFTextFieldDelegate!
     var sectionTitles: [String]?
     
+    var formController: YFormController!
+    
+    init(formController: YFormController) {
+        self.formController = formController
+    }
+    
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        var cell = collectionView.dequeueReusableCellWithReuseIdentifier(YFCollectionViewDataSource.cellIdentifier, forIndexPath: indexPath) as? YFFieldCell
+        
         let field = sections[indexPath.section].fields[indexPath.row]
-        field.textField = cell?.textField
-        cell?.textField.text = field.text
-        cell?.textField.placeholder = field.placeHolder
-        cell?.textField.delegate = textFieldDelegate
-        cell?.textField.errorMessage = field.errorMessage
-        cell?.textField.indexPath = indexPath
-        return cell!
+        
+        switch field.type {
+        case .Photo:
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier(XYZCells.PhotoField, forIndexPath: indexPath) as? YFPhotoFieldCell
+            cell?.viewController = formController
+            cell?.delegate = self
+            return cell!
+        case .Tags:
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier(XYZCells.TagField, forIndexPath: indexPath) as? YFTagFieldCell
+            cell?.tagWriteView.delegate = self
+            return cell!
+        case .Multiline:
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier(XYZCells.MultilineField, forIndexPath: indexPath) as? XYZMultilineFieldCell
+            return cell!
+        case .Switch:
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier(XYZCells.SwitchField, forIndexPath: indexPath) as? XYZSwitchFieldCell
+            cell?.delegate = self
+            cell?.indexPath = indexPath
+            return cell!
+        case .Date:
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier(XYZCells.DateField, forIndexPath: indexPath) as? XYZDateFieldCell
+            cell?.textField.delegate = textFieldDelegate
+            cell?.textField.indexPath = indexPath
+            return cell!
+        default:
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier(XYZCells.TextField, forIndexPath: indexPath) as? YFFieldCell
+            // todo: RENAME TEXTFIELD
+            if let textField = field as? YFTextField {
+                textField.textField = cell?.textField
+                if let text = textField.value as? String {
+                    cell?.textField.text = text
+                }
+                cell?.textField.placeholder = textField.placeHolder
+                cell?.textField.delegate = textFieldDelegate
+                cell?.textField.errorMessage = textField.errorMessage
+                cell?.textField.indexPath = indexPath
+                
+            }
+            
+            return cell!
+        }
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -55,7 +170,7 @@ class YFCollectionViewDataSource: NSObject, UICollectionViewDataSource {
     }
     
     func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
-        let headerView = collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionHeader, withReuseIdentifier: YFCollectionViewDataSource.headerIdentifier, forIndexPath: indexPath) as! YFFieldHeader
+        let headerView = collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionHeader, withReuseIdentifier: XYZCells.Header, forIndexPath: indexPath) as! YFFieldHeader
         headerView.titleLabel.text = sectionTitles![indexPath.section]
         return headerView
     }
@@ -66,6 +181,37 @@ class YFCollectionViewDataSource: NSObject, UICollectionViewDataSource {
             sections.insert(YFSection(), atIndex: section)
         }
         sections[section].fields.append(newField)
+    }
+    
+    // MARK: TagWriteViewDelegate
+    
+    func tagWriteViewDidEndEditing(view: TagWriteView!) {
+        println(view.tags)
+        //        formController.delegate?.formController(formController, updatedField: view.superview, atIndexPath: NSIndexPath(forRow: 0, inSection: 0))
+    }
+    
+    func tagWriteView(view: TagWriteView!, didRemoveTag tag: String!) {
+        println(view.tags)
+    }
+    
+    
+    // MARK: ImagePickerManagerDelegate
+    
+    func photoFieldCellDelegate(cell: YFPhotoFieldCell, didFinishPickingImage image: UIImage!) {
+        cell.photoButton.setBackgroundImage(image, forState: UIControlState.Normal)
+        //        formController.delegate?.formController(formController, updatedField: <#YFBasicField#>, atIndexPath: <#NSIndexPath#>)
+    }
+    
+    func switchFieldCell(switchFieldCell: XYZSwitchFieldCell, newValue: Bool) {
+        let field = sections[switchFieldCell.indexPath.section].fields[switchFieldCell.indexPath.row]
+        field.value = newValue
+        formController.delegate?.formController(formController, updatedField: field, atIndexPath: (field.identifier, switchFieldCell.indexPath.section))
+    }
+    
+    func datePickerViewController(field: XYFDateField, date: NSDate) {
+        field.value = date
+        formController.delegate?.formController(formController, updatedField: field, atIndexPath: (field.identifier, 0))
+        // todo: fill text field
     }
     
 }
@@ -82,8 +228,13 @@ class YFTextFieldDelegate: NSObject, UITextFieldDelegate {
     func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
         if let textField = textField as? YTextField {
             let field = formController.collectionViewDataSource!.sections[textField.indexPath.section].fields[textField.indexPath.row]
-            // Should show selector?
-            if field.shouldShowSelector {
+            if let dateField = field as? XYFDateField {
+                let datePickerViewController = DatePickerViewController()
+                datePickerViewController.field = dateField
+                datePickerViewController.delegate = formController.collectionViewDataSource
+                dateField.viewController.presentViewController(datePickerViewController, animated: true, completion: nil)
+                return false
+            } else if field.shouldShowSelector {
                 field.showSelector!()
                 return false
             } else {
@@ -97,12 +248,12 @@ class YFTextFieldDelegate: NSObject, UITextFieldDelegate {
     // Apply text from textField in the correct Form Field. Show error message if needed. Call form's delegate if no error.
     func textFieldDidEndEditing(textField: UITextField) {
         if let textField = textField as? YTextField {
-            let field = formController.collectionViewDataSource!.sections[textField.indexPath.section].fields[textField.indexPath.row]
-            field.text = textField.text
+            let field = formController.collectionViewDataSource!.sections[textField.indexPath.section].fields[textField.indexPath.row] as! YFTextField
+            field.value = textField.text
             field.errorMessage = YFValidationUtilities.errorMessageForText(textField.text, withValidation: field.validation)
             (textField as! FloatLabelTextField).errorMessage = field.errorMessage
             if field.errorMessage == nil {
-                formController.delegate?.formController(formController, updatedField: field as YFBasicField, atIndexPath: textField.indexPath)
+                formController.delegate?.formController(formController, updatedField: field, atIndexPath: (field.identifier, textField.indexPath.section))
             }
         } else {
             assertionFailure("textField should always be YTextField")
@@ -193,3 +344,22 @@ class YFKeyboardHandler: NSObject {
     }
     
 }
+
+struct XYZCells {
+    static let TextField = "YFFieldCell"
+    static let Header = "YFFieldHeader"
+    static let TagField = "YFTagFieldCell"
+    static let PhotoField = "YFPhotoFieldCell"
+    static let MultilineField = "XYZMultilineFieldCell"
+    static let SwitchField = "XYZSwitchFieldCell"
+    static let DateField = "XYZDateFieldCell"
+    
+}
+
+class XYZCollectionViewCell: UICollectionViewCell {
+    
+    var indexPath: NSIndexPath!
+    
+}
+
+typealias XYZIndexPath = (String, Int)
