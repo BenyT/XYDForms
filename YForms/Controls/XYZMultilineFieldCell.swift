1856db5a89137eb669e299507a5d5507eb0fb296
Diogo Mateus <diogo.tpr@gmail.com>
2015-09-22 22:41:27 +0100

    Added a multiline field

---
+++ YForms/Controls/XYZMultilineFieldCell.swift
@@ -0,0 +1,18 @@
+//
+//  XYZMultilineFieldCell.swift
+//  MealsApp
+//
+//  Created by Meals App on 20/09/15.
+//  Copyright (c) 2015 mateusd. All rights reserved.
+//
+
+import UIKit
+
+class XYZMultilineFieldCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
