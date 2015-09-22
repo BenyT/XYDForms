e83eb1553a6c1c487e1245fa0916e1869aa4c8bd
Diogo Mateus <diogo.tpr@gmail.com>
2015-09-22 23:57:35 +0100

    Fixed header comments

--- YForms/Model/XYDSection.swift
+++ YForms/Model/XYDSection.swift
@@ -1,8 +1,8 @@
 //
 //  XYDSection.swift
-//  YFormsDemo
+//  XYDForms
 //
-//  Created by Meals App on 22/09/15.
+//  Created by Mateus, Diogo on 22/09/15.
 //  Copyright (c) 2015 DiogoMateus. All rights reserved.
 //
 
--- YForms/XYDValidator.swift
+++ YForms/XYDValidator.swift
@@ -1,6 +1,6 @@
 //
 //  XYDValidator.swift
-//  YForms
+//  XYDForms
 //
 //  Created by Mateus, Diogo on 12/08/15.
 //  Copyright (c) 2015 Mateus, Diogo. All rights reserved.
