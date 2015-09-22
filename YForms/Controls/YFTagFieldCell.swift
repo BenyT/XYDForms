aab5efafff7037a236df94fe2f508768ec632415
Diogo Mateus <diogo.tpr@gmail.com>
2015-09-22 22:39:04 +0100

    Added a tags field

---
+++ YForms/Controls/YFTagFieldCell.swift
@@ -0,0 +1,20 @@
+//
+//  YFTagFieldCell.swift
+//  MealsApp
+//
+//  Created by Meals App on 06/09/15.
+//  Copyright (c) 2015 mateusd. All rights reserved.
+//
+
+import UIKit
+
+class YFTagFieldCell: UICollectionViewCell {
+
+    @IBOutlet weak var tagWriteView: TagWriteView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+}
