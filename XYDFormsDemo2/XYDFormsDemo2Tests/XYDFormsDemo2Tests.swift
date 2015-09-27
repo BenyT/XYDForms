9580ddbe2d2ce1d8c4d7101a9e12db5d1fdcf469
Diogo Mateus <mateusd@choitmateusd.local>
2015-09-27 18:28:54 +0100

    Added Demo2

---
+++ XYDFormsDemo2/XYDFormsDemo2Tests/XYDFormsDemo2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  XYDFormsDemo2Tests.swift
+//  XYDFormsDemo2Tests
+//
+//  Created by Mateus, Diogo on 27/09/2015.
+//  Copyright (c) 2015 Diogo Mateus. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class XYDFormsDemo2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
